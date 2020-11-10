7431ce338dae49d156b1f5e901a8abf55a74f664
danilo <daniloborges.cg@gmail.com>
2020-11-10 18:50:17 -0300

    sugestao

--- 3/function_of_motion_correct.py
+++ 3/function_of_motion_correct.py
@@ -10,6 +10,14 @@ def motionOne(mass, force_constant, time):
     return  [int( sin(frequency*time) * ((11*force_constant) - (pow(frequency,2)*mass))*(u) - (force_constant*u)),\
              int( sin(frequency*time) * (force_constant*u) - ((11*force_constant) - (pow(frequency,2)*mass))*(u))]
 
+
+#SUGESTÃO utilizar a equação de definição do movimento
+#    frequency = 21.9
+#    u = 1/sqrt(2)
+#    return  [u* sin(frequency*time),\
+#             -u* sin(frequency*time)]
+
+
 # def motionSecond(mass, force_constant, time):
 #     """
 #     You will got the position of the mass.
